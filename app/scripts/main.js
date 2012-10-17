b72f544fdd651e764afa574bd83265b77219bc50
huang47 <huge.huang@gmail.com>
2012-10-17 20:50:29 +0800

    use yeoman to bootstrap application

---
+++ app/scripts/main.js
@@ -0,0 +1,45 @@
+require.config({
+  shim: {
+  },
+
+  paths: {
+    hm: 'vendor/hm',
+    esprima: 'vendor/esprima',
+    jquery: 'vendor/jquery.min'
+  }
+});
+ 
+require(['app'], function(app) {
+    var graph = new Graph(),
+        s1 = graph.newNode({ label: '綠咖哩起士烤鮮魚', image: '/images/s1.jpg' } ),
+        s2 = graph.newNode({ label: '沙嗲活菌豬串燒', image: '/images/s2.jpg' } ),
+        s3 = graph.newNode({ label: '起士焗烤蟹肉', image: '/images/s3.jpg' } ),
+        s4 = graph.newNode({ label: '培根起士焗磨菇', image: '/images/s4.jpg' } ),
+        s5 = graph.newNode({ label: '義式生牛肉', image: '/images/s5.jpg' } ),
+        s6 = graph.newNode({ label: '煙燻鮭魚沙拉', image: '/images/s6.jpg' } ),
+        s7 = graph.newNode({ label: '義大利香煎雞柳', image: '/images/s7.jpg' } );
+
+    graph.newEdge(s1, s2, { color: '#333333', directional: false });
+    graph.newEdge(s1, s3, { color: '#333333', directional: false });
+    graph.newEdge(s1, s5, { color: '#333333', directional: false });
+    graph.newEdge(s2, s5, { color: '#333333', directional: false });
+    graph.newEdge(s2, s6, { color: '#333333', directional: false });
+    graph.newEdge(s3, s4, { color: '#333333', directional: false });
+    graph.newEdge(s3, s5, { color: '#333333', directional: false });
+    graph.newEdge(s3, s7, { color: '#333333', directional: false });
+    graph.newEdge(s4, s6, { color: '#333333', directional: false });
+    graph.newEdge(s5, s7, { color: '#333333', directional: false });
+
+    jQuery(function(){
+        var springy = jQuery('#springydemo').springy({
+            graph: graph,
+            nodeSelected: function(node) {
+                var s8 = graph.newNode({ label: '煙燻鮭魚沙拉 1', image: '/images/s6.jpg' }),
+                    s9 = graph.newNode({ label: '煙燻鮭魚沙拉 2', image: '/images/s6.jpg' });
+
+                graph.newEdge(s6, s8, { color: '#ff0000', directional: false });
+                graph.newEdge(s6, s9, { color: '#ff0000', directional: false });
+            }
+        });
+    });
+});
